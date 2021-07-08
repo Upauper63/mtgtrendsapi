7190d25092199a22d4b130fcf1dcb412befcb8f5
Upauper63 <syunufkin@gmail.com>
2021-07-09 03:41:11 +0900

    scrape scripts 途中まで

--- mtgtrendsapi/models.py
+++ mtgtrendsapi/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Scrapes(models.Model):
+    date = models.DateField(auto_now_add=True)
+    started_at = models.DateTimeField(auto_now_add=True)
+    finished_at = models.DateTimeField(null=True)
+    is_finished = models.BooleanField(null=True)
+
+class Items(models.Model):
+    product_id = models.IntegerField(db_index=True, blank=False, null=False)
+    name = models.CharField(max_length=255, blank=False)
+    diff_price_prev = models.IntegerField(null=True)
+    scrape_0 = models.ForeignKey(Scrapes, related_name='scrape_0', on_delete=models.CASCADE, null=True)
+    price_0 = models.IntegerField(null=True)
+    scrape_1 = models.ForeignKey(Scrapes, related_name='scrape_1', on_delete=models.CASCADE, null=True)
+    price_1 = models.IntegerField(null=True)
+    scrape_2 = models.ForeignKey(Scrapes, related_name='scrape_2', on_delete=models.CASCADE, null=True)
+    price_2 = models.IntegerField(null=True)
+    scrape_3 = models.ForeignKey(Scrapes, related_name='scrape_3', on_delete=models.CASCADE, null=True)
+    price_3 = models.IntegerField(null=True)
+    def __str__(self):
+        return self.name
---
+++ mtgtrendsapiproject/scripts/scrape.py
@@ -0,0 +1,90 @@
+from mtgtrendsapi.models import Items, Scrapes
+from bs4 import BeautifulSoup
+import datetime
+import urllib.request
+import time
+import re
+
+import sys
+sys.path.append('../')
+from mtgtrendsapi.models import Items, Scrapes
+# from ...mtgtrendsapi.models import Items, Scrapes
+
+def run():
+    scrape_obj = Scrapes()
+    scrape_obj.save()
+    scrape_id = scrape_obj.id
+    x = (scrape_id - 1) % 4
+    # 英語 ノーマル 値段高い順にソート
+    ori_url = "https://www.hareruyamtg.com/ja/products/search?foilFlg%5B0%5D=0&language%5B0%5D=2&sort=price&order=DESC&page="
+    items_class = ".itemList"
+    name_class = '.itemName'
+    price_class = '.itemDetail__price'
+    url_class = '.itemData > a'
+    existPage = True
+    i = 1
+    while existPage == True:
+        try:
+            url = ori_url + str(i)
+            headers = {
+                "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0",
+            }
+            request = urllib.request.Request(url=url, headers=headers)
+            response = urllib.request.urlopen(request)
+            html = response.read()
+            soup = BeautifulSoup(html, "html.parser")
+            items = soup.select(items_class)
+            # 終了判定
+            if len(items) == 0:
+                existPage = False
+                continue
+            for item in items:
+                name = item.select(name_class)[0].text.strip()
+                price = item.select(price_class)[0].text.strip()
+                url = item.select(url_class)[0].get('href')
+                seg = 'detail/'
+                product_id = int(url[url.find(seg)+len(seg):url.find('?')])
+                # 値段の成形
+                price = int(re.sub(r"\D",'', price))
+
+                obj = ""
+                obj = Items.objects.filter(product_id=product_id).first()
+                if obj.name:
+                    prev_x = (x - 1) % 4
+                    dic = obj.__dict__
+                    dic.pop('_state')
+                    price_diff_prev = price - dic['price_' + str(prev_x)]
+                    kwargs = dic
+                    kwargs['scrape_' + str(x)] = scrape_obj
+                    kwargs['price_' + str(x)] = price
+                    kwargs['price_diff_prev'] = price_diff_prev
+                    Items.objects.filter(product_id=product_id).delete()
+                    obj = Items(**kwargs)
+                else:
+                    kwargs = {'product_id': product_id, 'name': name, 'scrape_' + str(x): scrape_obj, 'price_' + str(x): price}
+                    obj = Items(**kwargs)
+                if obj:
+                    # レコード数に制限があるため、対象データに制限
+                    if price < 300:
+                        existPage = False
+                    elif price <= 100000:
+                        obj.save()
+                else:
+                    existPage = False
+
+            i += 1
+            time.sleep(2)
+
+        except urllib.error.HTTPError as err:
+            print("{}-ErrorOccured:{}".format(err.code))
+            existPage = False
+        except urllib.error.URLError as err:
+            print("{}-ErrorOccured:{}".format(err.reason))
+            existPage = False
+        except:
+            existPage = False
+            
+    else:
+        scrape_obj.is_finished = True
+        scrape_obj.finished_at = datetime.datetime.now()
+        scrape_obj.save()
