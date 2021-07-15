98f7f897073deff0fc95d3dc748c823558f0d778
Upauper63 <syunufkin@gmail.com>
2021-07-16 08:51:13 +0900

    読み込み専用のオプションを追加

--- mtgtrendsapi/serializer.py
+++ mtgtrendsapi/serializer.py
@@ -6,11 +6,13 @@ class ItemsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = ('id', 'name', 'diff_price_prev')
+        read_only_fields = [f.name for f in Item._meta.get_fields()]
 
 class ItemSerializer(serializers.ModelSerializer):
     trends = serializers.SerializerMethodField()
     class Meta:
         model = Item
         fields = ('id', 'name', 'diff_price_prev', 'trends')
+        read_only_fields = [f.name for f in Item._meta.get_fields()]
     def get_trends(self, instance):
         return instance.get_trends()
