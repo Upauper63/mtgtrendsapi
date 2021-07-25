ac72446288c9f0810ed1c5fa23b36e8a4a2b3b72
Upauper63 <syunufkin@gmail.com>
2021-07-25 13:06:58 +0900

    管理サイトへモデル登録

--- mtgtrendsapi/admin.py
+++ mtgtrendsapi/admin.py
@@ -1,3 +1,7 @@
+from mtgtrendsapi.models import Item, Scrape
 from django.contrib import admin
+from .models import Scrape, Item
 
 # Register your models here.
+admin.site.register(Scrape)
+admin.site.register(Item)
